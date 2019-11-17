cdf8f28882c67fcd43a82a63dabea38410c45727
A Anand <33590033+a-anand-91119@users.noreply.github.com>
2019-11-17 19:14:54 +0530

    Fixed StackOverFlow Error in larger puzzles

--- SudokuSolverBackTrackMethod/src/com/anand/sudoku/solver/algorithm/Solver.java
+++ SudokuSolverBackTrackMethod/src/com/anand/sudoku/solver/algorithm/Solver.java
@@ -7,30 +7,33 @@ public class Solver {
 
 	private SudokuBoard sudokuBoard = null;
 	private boolean solutionFound = false;
+	private int size;
 
 	public Solver initialize(String sudokuData, int row, int innerRow, int innerColumn) {
 		sudokuBoard = new SudokuBoard(sudokuData, row, innerRow, innerColumn);
+		size = row;
 		return this;
 	}
 
 	public void solve(int solutionType) throws BoardNotInitializedException {
 		if (sudokuBoard == null)
 			throw new BoardNotInitializedException("The Sudoku Board Is Not Initialized Properly.");
-		
-		if(solutionType == SolverConstants.SINGLE_SOLUTION) {
+
+		if (solutionType == SolverConstants.SINGLE_SOLUTION) {
 			long startTime = System.currentTimeMillis();
-			if(!doSolveOneSolution(sudokuBoard))
+			if (!doSolveOneSolution(sudokuBoard))
 				System.out.println("No Solution Found");
 			System.out.println("Time Elapsed To Find Solution: " + (System.currentTimeMillis() - startTime) + "ms");
-		}else if(solutionType == SolverConstants.ALL_SOLUTIONS) {
+		} else if (solutionType == SolverConstants.ALL_SOLUTIONS) {
 			long startTime = System.currentTimeMillis();
-			if(!doSolveAllPossibleSolutions(sudokuBoard))
+			if (!doSolveAllPossibleSolutions(sudokuBoard))
 				System.out.println("No Solution Found");
-			System.out.println("Time Elapsed To Find All Possible Solutions: " + (System.currentTimeMillis() - startTime) + "ms");
+			System.out.println(
+					"Time Elapsed To Find All Possible Solutions: " + (System.currentTimeMillis() - startTime) + "ms");
 		}
 	}
 
-	private boolean doSolveOneSolution(SudokuBoard sudokuBoard2) {
+	private boolean doSolveOneSolution(SudokuBoard sudokuBoard) {
 		int i = 0;
 		int j = 0;
 		
@@ -38,9 +41,8 @@ public class Solver {
 			sudokuBoard.printBoard();
 			return true;
 		} else {
-			outerLoop:
-			for (i = 0; i < 9; i++) {
-				for (j = 0; j < 9; j++) {
+			outerLoop: for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
 					if (sudokuBoard.isNotFilled(i, j)) {
 						break outerLoop;
 					}
@@ -49,13 +51,13 @@ public class Solver {
 
 			int[] possibilities = sudokuBoard.possibleEntries(i, j);
 			for (int index = 0; index < possibilities.length; index++) {
-				if(possibilities[index] != 0) {
-					sudokuBoard.set(i,j,possibilities[index]);
-					if(doSolveOneSolution(sudokuBoard))
+				if (possibilities[index] != 0) {
+					sudokuBoard.set(i, j, possibilities[index]);
+					if (doSolveOneSolution(sudokuBoard))
 						return true;
 				}
 			}
-			sudokuBoard.set(i,j,0);
+			sudokuBoard.set(i, j, 0);
 		}
 		return false;
 	}
@@ -63,16 +65,15 @@ public class Solver {
 	private boolean doSolveAllPossibleSolutions(SudokuBoard sudokuBoard) {
 		int i = 0;
 		int j = 0;
-		
+
 		if (sudokuBoard.isFull()) {
 			System.out.println("Solved");
 			sudokuBoard.printBoard();
 			solutionFound = true;
 			return true;
 		} else {
-			outerLoop:
-			for (i = 0; i < 9; i++) {
-				for (j = 0; j < 9; j++) {
+			outerLoop: for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
 					if (sudokuBoard.isNotFilled(i, j)) {
 						break outerLoop;
 					}
@@ -81,12 +82,12 @@ public class Solver {
 
 			int[] possibilities = sudokuBoard.possibleEntries(i, j);
 			for (int index = 0; index < possibilities.length; index++) {
-				if(possibilities[index] != 0) {
-					sudokuBoard.set(i,j,possibilities[index]);
+				if (possibilities[index] != 0) {
+					sudokuBoard.set(i, j, possibilities[index]);
 					doSolveAllPossibleSolutions(sudokuBoard);
 				}
 			}
-			sudokuBoard.set(i,j,0);
+			sudokuBoard.set(i, j, 0);
 		}
 		return solutionFound;
 	}
